9fb70733f42ab19605716947bb8c70ce2876992a
Debojyoti-Sarkar-07 <sdebojyoti170083@gmail.com>
2024-09-25 11:48:24 +0530

    Check odd no. or even no. in flutter.dart

--- flutter.dart
+++ flutter.dart
@@ -1,3 +1,38 @@
-void main(){
-  
+void main() {
+  int a = 5;
+  int b = 7;
+  int c = 787;
+  int d = 64;
+  int e = 752;
+  if (a % 2 == 0) {
+    print("$a is an even number.");
+  } else if (a % 2 != 0) {
+    print("$a is a odd umber.");
+  }
+
+  if (b % 2 == 0) {
+    print("$b is an even number.");
+  } else if (b % 2 != 0) {
+    print("$b is a odd number.");
+  }
+
+  if (c % 2 == 0) {
+    print("$c is an even number.");
+  } else if (c % 2 != 0) {
+    print("$c is a odd number.");
+  }
+
+  if (d % 2 == 0) {
+    print("$d is an even number.");
+  } else if (d % 2 != 0) {
+    print("$d is a odd number.");
+  }
+
+  if (e % 2 == 0) {
+    print("$e is an even number.");
+  } else if (e % 2 != 0) {
+    print("$e is a odd number.");
+  }
+
+  // Using ternary operator to check if a number is even or odd
 }
